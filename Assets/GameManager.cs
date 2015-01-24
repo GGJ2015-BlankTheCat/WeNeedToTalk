0072e402254fd6813e512c5befe73239c435f8bc
jcoates <jcoates@users.noreply.github.com>
2015-01-24 14:35:41 -0500

    Adds the files I failed to add

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	GameObject chatHandler;
+	ChatParser chatParser;
+
+	enum GameState{Choice, Dialogue};
+
+	public GameState currentState;
+
+
+	// Use this for initialization
+	void Awake () {
+		currentState = GameState.Dialogue;
+		chatHandler = GameObject.Find ("ChatHandler");
+		chatParser = chatHandler.GetComponent<ChatParser> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentState == GameState.Choice) {
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				return;
+			} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+				return;
+			} else if (Input.GetKeyDown (KeyCode.Space)) {
+				return;
+			}
+		} else if (currentState == GameState.Dialogue) {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				chatParser.NextLine();
+				if (chatParser.IsLastLine()) {
+					currentState = GameState.Choice;
+				}
+			} else {
+				return;
+			}
+		}
+	}
+}
