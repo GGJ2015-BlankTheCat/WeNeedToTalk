50335c0c3672218d794b7c7a188d0ffe7817f3f6
jcoates <jcoates@users.noreply.github.com>
2015-01-24 14:32:10 -0500

    Adds Assets and ChatParser

--- Assets/Scripts/ChatParser.cs
+++ Assets/Scripts/ChatParser.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -8,15 +9,20 @@ using System.Text;
 public class ChatParser : MonoBehaviour {
 
 	GameObject gameManager;
+	GameObject textBox;
+	GameObject notePad;
 	public Dictionary<string, tweeEntry> tweeEntries; 
 	
 
 	public tweeEntry currentEntry;
 	public int CurrentLine;
-
+	public int lastBodyLine;
+	
 	void Awake(){
 		gameManager = GameObject.Find ("gameManager");
 		tweeEntries = gameManager.GetComponent<TweeParser> ().entries;
+		textBox = GameObject.Find ("TextBox");
+		notePad = GameObject.Find ("Note Pad");
 
 		CurrentLine = 0;
 
@@ -24,16 +30,32 @@ public class ChatParser : MonoBehaviour {
 
 	void NextTwee(string title){
 		currentEntry = tweeEntries [title];
+		CurrentLine = 0;
+
+		int lineCtr = 0;
+		foreach (string line in currentEntry.body) {
+			if(line.Trim ().IndexOf ('[') == 0) {
+				break;
+			}
+			lineCtr++;
+		}
+		//This should return the last line which isn't an option
+		lastBodyLine = lineCtr--;
+	}
+	
+	public void NextLine() {
+		CurrentLine++;
 	}
 
-
-
+	public bool IsLastLine() {
+		return CurrentLine == lastBodyLine);
+	}
 
 	void OnGui(){
-
+		textBox.GetComponent<Text> ().text = currentEntry.body [CurrentLine];
 	}
 
-	void Update(){
-	}
+	void RenderNotePad() {
 
+	}
 }
