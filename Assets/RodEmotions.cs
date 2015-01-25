ff4a7a382f51a09dd9dbc9a35f4bd4cd04a43f93
jcoates <jccoates@ccs.neu.edu>
2015-01-25 12:10:57 -0500

    Adds Rod

---
+++ Assets/RodEmotions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RodEmotions : MonoBehaviour {
+	
+	GameObject rodSpriteRenderer;
+	SpriteRenderer rodSprite;
+	
+	public Sprite rodHappy;
+	public Sprite rodSad;
+	public Sprite rodAngry;
+	public Sprite rodNeutral;
+	
+	// Use this for initialization
+	void Awake () {
+		GameObject.Find("RodSprite").GetComponent<SpriteRenderer>().sprite = rodNeutral;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void SetEmotion(Emotion emot) {
+		switch (emot) {
+		case Emotion.Angry:
+			GameObject.Find ("Rod").GetComponent<SpriteRenderer> ().sprite = rodAngry;
+			break;
+		case Emotion.Happy:
+			GameObject.Find ("Rod").GetComponent<SpriteRenderer> ().sprite = rodHappy;
+			break;
+		case Emotion.Neutral:
+			GameObject.Find ("Rod").GetComponent<SpriteRenderer> ().sprite = rodNeutral;
+			break;
+		case Emotion.Sad:
+			GameObject.Find ("Rod").GetComponent<SpriteRenderer> ().sprite = rodSad;
+			break;
+		}
+	}
+}
