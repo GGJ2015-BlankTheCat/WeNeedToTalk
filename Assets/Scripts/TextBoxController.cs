15c3d73f9a0bed6612d0d75e2de77d9085f9faf4
Nathan Holmes <holmes.nathan.d@gmail.com>
2015-01-25 12:00:52 -0500

    i dont rememer what i did

--- Assets/Scripts/TextBoxController.cs
+++ Assets/Scripts/TextBoxController.cs
@@ -7,6 +7,9 @@ using System.Text;
 
 public class TextBoxController : MonoBehaviour {
 
+	public Sprite blankSprite;
+	public Sprite raySprite;
+	public Sprite rodSprite;
 
 
 	GameObject gameManagerObject;
@@ -15,6 +18,7 @@ public class TextBoxController : MonoBehaviour {
 	GameObject textInterfaceObject;
 	ChatHandler chatHandlerScript;
 
+	GameObject textboxObject;
 	GameObject chatTextObject;
 
 
@@ -29,8 +33,9 @@ public class TextBoxController : MonoBehaviour {
 		chatHandlerScript = textInterfaceObject.GetComponent<ChatHandler> ();
 
 		chatTextObject = GameObject.Find ("ChatText");
+		textboxObject = GameObject.Find ("TextBox");
 
-
+		
 	}
 	
 	// Update is called once per frame
@@ -44,10 +49,26 @@ public class TextBoxController : MonoBehaviour {
 	void OnGUI() {
 		if (currentState == GameState.Dialogue) {
 
-			chatTextObject.GetComponent<Text>().text = chatHandlerScript.GetCurrentLine();
+			textboxObject.transform.localScale = new Vector3(1,1,1);
+
+
+			if (chatHandlerScript.GetCurrentLine ().Contains ("Ray:")) {
+				textboxObject.GetComponent<Image> ().sprite = raySprite;
+			} else if (chatHandlerScript.GetCurrentLine ().Contains ("Rod:")) {
+				textboxObject.GetComponent<Image> ().sprite = rodSprite;
+			} else {
+			 	textboxObject.GetComponent<Image> ().sprite = blankSprite;
+			}
+
+
+			chatTextObject.GetComponent<Text> ().text = chatHandlerScript.GetCurrentLine ();
 				
 
-			}
+		
+		} else if (currentState == GameState.Intro) {
+
+			textboxObject.transform.localScale = new Vector3(0,0,0);
+		}
 
 	}
 }
